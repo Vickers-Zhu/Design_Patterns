1f2e93114805d57d809a5e3abcdbfce608ae7e7e
Yyering <29935648+Vickers-Zhu@users.noreply.github.com>
2020-04-24 22:30:31 +0200

    2nd proj added

---
+++ DataFrames/DataFrame.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFrames
+{
+    public class Record
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public Record()
+        { }
+        public Record(string name, string surname, string gender, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            Age = age;
+        }
+    }
+
+    public class ListDataFrame : IEnumerable<Record>
+    {
+        List<Record> list;
+
+        public double length
+        {
+            get
+            {
+                return list.Count;
+            }
+        }
+
+        public ListDataFrame(List<Record> list)
+        {
+            this.list = list;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerator<Record> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+    }
+
+    public class FileDataFrameEnum : IEnumerator<Record>
+    {
+        List<Record> list;
+
+        StreamReader sr;
+
+        string filename;
+        string obs;
+
+        int position = -1;
+
+        public FileDataFrameEnum(string filename)
+        {
+            this.filename = filename;
+            sr = new StreamReader(filename);
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public Record Current
+        {
+            get
+            {
+                try
+                {
+                    string[] record= obs.Split(',');
+                    return new Record(record[0].Trim(), record[1].Trim(), record[2].Trim(), int.Parse(record[3]));
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return ((obs = sr.ReadLine()) != null);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+            sr = new StreamReader(filename);
+        }
+
+        public void Dispose()
+        {
+            sr.Close();
+        }
+    }
+
+    public class FileDataFrame : IEnumerable<Record>
+    {
+        string filename;
+
+        public FileDataFrame(string filename)
+        {
+            this.filename = filename;
+        }
+
+        public FileDataFrame(IEnumerable<Record> results, string filename)
+        {
+            using (StreamWriter sw = new StreamWriter(filename))
+            {
+                foreach (Record obs in results)
+                {
+                    sw.WriteLine($"{obs.Name}, {obs.Surname}, {obs.Gender}, {obs.Age}");
+                }
+                sw.Close();
+            }
+        }
+
+        public IEnumerator<Record> GetEnumerator()
+        {
+            return new FileDataFrameEnum(filename);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ DataFrames/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataFrames
+{
+    class Program
+    {
+        static void PrintDataFrame(IEnumerable<Record> pdf)//...
+        {
+            foreach (Record obs in pdf)
+            {
+                Console.WriteLine(obs.Name + ", " + obs.Surname + ", " + obs.Gender + ", " + 
+                    obs.Age);
+            }
+        }
+
+        static Tuple<string, double> Mode(IEnumerable<Record> pdf)
+        {
+            double dif = 0;
+            double length = 0;
+            foreach(Record obs in pdf)
+            {
+                length++;
+                if (obs.Gender == "M")
+                    dif++;
+                else dif--;
+            }
+            if (dif > 0)
+                return new Tuple<string, double>("M", (length + dif) / (2 * length));
+            else
+                return new Tuple<string, double>("F", (length - dif) / (2 * length));
+        }
+
+
+
+        static List<Record> ConcatenationWithCondition(IEnumerable<Record> ldf, IEnumerable<Record> fdf, Func<Record, bool> f)
+        {
+            List<Record> results = new List<Record>();
+            foreach (Record obs in ldf)
+            {
+                if (f(obs))
+                    results.Add(obs);
+            }
+            foreach (Record obs in fdf)
+            {
+                if (f(obs))
+                    results.Add(obs);
+            }
+            return results;
+
+        }
+
+        static void Main(string[] args)
+        {
+            List < Record > people = new List<Record>()
+            {
+                new Record("Keanu", "Reeves", "M", 25),
+                new Record("Agent", "Smith", "M", 35),
+                new Record("Carrie-Anne", "Moss", "F", 25),
+                new Record("Gloria", "Foster", "M", 123),
+                new Record("Belinda", "McClory", "F", 27)
+            };
+            ListDataFrame ldf = new ListDataFrame(people);
+            PrintDataFrame(ldf);
+
+            Tuple<string, double> res = Mode(ldf);
+            Console.WriteLine($"mode = {res.Item1}, percent = {res.Item2}");
+
+            FileDataFrame fdf = new FileDataFrame("dataFrame1.txt");
+            PrintDataFrame(fdf);
+            Tuple<string, double> res2 = Mode(fdf);
+            Console.WriteLine($"mode = {res2.Item1}, percent = {res2.Item2}");
+            Console.WriteLine("\nConcatenation\n");
+            Func<Record, bool> f = r => r.Gender == "F";
+            PrintDataFrame(new ListDataFrame(ConcatenationWithCondition(ldf, fdf, f)));
+            FileDataFrame fdf2 = new FileDataFrame(ConcatenationWithCondition(ldf, fdf, f), "dataFrame2.txt");
+            Console.ReadKey();
+        }
+    }
+}
