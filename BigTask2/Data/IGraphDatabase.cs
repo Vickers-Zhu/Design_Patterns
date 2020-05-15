51f00cb76fe69b50a4ad38e8dd3e3d79f480c937
Yyering <29935648+Vickers-Zhu@users.noreply.github.com>
2020-05-15 14:31:59 +0200

    Initial commit for bigtask2

---
+++ BigTask2/Data/IGraphDatabase.cs
@@ -0,0 +1,12 @@
+﻿//This file contains fragments that You have to fulfill
+
+using BigTask2.Api;
+namespace BigTask2.Data
+{
+    public interface IGraphDatabase
+    {
+        //Fill the return type of the method below
+        /* */ void GetRoutesFrom(City from);
+        City GetByName(string cityName);
+    }
+}
---
+++ BigTask2/Program.cs
@@ -0,0 +1,87 @@
+﻿using BigTask2.Api;
+using BigTask2.Data;
+using BigTask2.Ui;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BigTask2
+{
+	class Program
+	{
+        static IEnumerable<Route> ServeRequest(Request request)
+        {
+            (IGraphDatabase cars, IGraphDatabase trains) = MockData.InitDatabases();
+
+            /*
+			 *
+			 * Add request handling here and return calculated route
+			 *
+			 */
+            return null;
+		}
+		static void Main(string[] args)
+		{
+            Console.WriteLine("---- Xml Interface ----");
+            /*
+			 * Create XML System Here
+             * and execute prepared strings
+			 */
+            //Execute(xmlSystem, "xml_input.txt");
+            Console.WriteLine();
+
+            Console.WriteLine("---- KeyValue Interface ----");
+            /*
+			 * Create INI System Here
+             * and execute prepared strings
+			 */
+            //Execute(keyValueSystem, "key_value_input.txt");
+            Console.WriteLine();
+        }
+
+        /* Prepare method Create System here (add return, arguments and body)*/
+        /*static CreateSystem 
+        {
+			
+        }*/
+
+        static void Execute(ISystem system, string path)
+        {
+            IEnumerable<IEnumerable<string>> allInputs = ReadInputs(path);
+            foreach (var inputs in allInputs)
+            {
+                foreach (string input in inputs)
+                {
+                    system.Form.Insert(input);
+                }
+                var request = RequestMapper.Map(system.Form);
+                var result = ServeRequest(request);
+                system.Display.Print(result);
+                Console.WriteLine("==============================================================");
+            }
+        }
+
+        private static IEnumerable<IEnumerable<string>> ReadInputs(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                List<List<string>> allInputs = new List<List<string>>();
+                while (!file.EndOfStream)
+                {
+                    string line = file.ReadLine();
+                    List<string> inputs = new List<string>();
+                    while (!string.IsNullOrEmpty(line))
+                    {
+                        inputs.Add(line);
+                        line = file.ReadLine();
+                    }
+                    if (inputs.Count > 0)
+                    {
+                        allInputs.Add(inputs);
+                    }
+                }
+                return allInputs;
+            }
+        }
+    }
+}
